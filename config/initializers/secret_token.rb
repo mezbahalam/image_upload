0fa84219d53ebfc899d70b297466334cca588655
mezbahalam <mezbahalam26@gmail.com>
2014-08-20 13:18:03 +0600

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imageupload::Application.config.secret_key_base = 'fe0a917ca52ddaea9a76edd077ffe36265d576ac76ad2b334a1af613dc79c244df5f47a1a5ad8d38ed23e69881e1441b69ccac946c3f71dfff6b574cbefbe75c'
